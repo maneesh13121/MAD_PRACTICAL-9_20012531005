554fbfdf2201dd5d5e0344b254f7696f900a0c36
Maneesh Kumar <maneeshchintakindi20@gnu.ac.in>
2022-11-21 18:55:32 +0530

    Add files via upload

---
+++ SMSView.kt
@@ -0,0 +1,9 @@
+
+package com.example.a20012531005_practical_9_mad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class SMSView(val phoneNo:String,val msg:String) {
+}
